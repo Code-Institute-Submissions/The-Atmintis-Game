496adc8b7788622623b9a6413d426c28c583e4d3
Leefarmer83 <leefarmer83@gmail.com>
2020-11-16 16:02:44 +0000

    Added divs in index.html for messaging user to start,Gameover and congratulations message.added classes to style.css to grow and animate with a shield of a darker transparent blue

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,3 +4,13 @@ $(function () {
       trigger : 'hover'
   })
 })
+
+// see webdevsimplified as their documentation helped
+
+if (document.readyState == 'loading') { 
+    document.addEventListener('DOMContentLoaded', ready); // if loading wait until it loads and call ready
+} else {
+    ready();
+}
+
+// message screen for start game, game over and card functions, need to add the divs.
