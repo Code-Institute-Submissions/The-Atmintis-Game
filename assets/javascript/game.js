cfa03439afc33fba850094ba87d48da3542bff92
Leefarmer83 <leefarmer83@gmail.com>
2020-11-20 12:03:04 +0000

    add function audio controller with a constructor, this allows me to call audio sounds against objecys

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,6 +14,44 @@ if (document.readyState == 'loading') {
 }
 
 // message screen for start game, game over and card functions, need to add the divs.
+class AudioController {
+    constructor() {
+        this.countdownSound = new Audio('assets/audio/countdown.mp3');
+        this.turnSound = new Audio('assets/audio/turn.mp3');
+        this.pairSound = new Audio('assets/audio/pair.mp3');
+        this.winnerSound = new Audio('assets/audio/winner.mp3');
+        this.gameOverSound = new Audio('assets/audio/gameover.mp3');
+        // and language sound later
+        this.countdownSound.volume = 0.2;
+        this.countdownSound.loop = true;
+    }
+    startMusic() {
+        this.countdownSound.play();
+    }
+    stopMusic() {
+        this.countdownSound.pause();
+        this.countdownSound.currentTime = 0;
+    }
+    flip() {
+        this.turnSound.play();
+         
+    }
+    match() {
+        this.pairSound.play();
+        // play language sound this.language.play
+    }
+    victory() {
+        this.stopMusic();
+        this.winnerSound.play();
+    }
+    gameOver() {
+        this.stopMusic();
+        this.gameOverSound.play();
+    }
+}
+
+
+
 function ready() {
     let overlays = Array.from(document.getElementsByClassName('gametext'));
     let cards = Array.from(document.getElementsByClassName('card'));
@@ -22,6 +60,8 @@ function ready() {
     overlays.forEach(overlay => {
         overlay.addEventListener('click', () => {
             overlay.classList.remove('show');
+                let audioController = new AudioController();
+                audioController.startMusic();
             
         });
     });
